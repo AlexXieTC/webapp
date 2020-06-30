65393ee77dc4e5675fbcfc12209fab116e3be32f
y-hikino <y-hikino@vcl.jp>
2020-06-30 15:29:57 +0900

    DBManager、mappingの修正

--- newproject/WEB-INF/src/database/DBManager.java
+++ newproject/WEB-INF/src/database/DBManager.java
@@ -1,5 +1,89 @@
 package database;
 
-public class DBManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import resultmapping.ResultSetBeanMapping;
+
+public class DBManager{
+	//接続するデータベース名
+	private static final String DB="serversidedb";
+	//各ユーザのパスワード
+	private static final String  PASS ="";
+
+	public static Connection getConnection() throws SQLException{
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection con=DriverManager.getConnection(
+			"jdbc:postgresql://localhost:5432/"+DB,
+			"postgres",PASS);
+			return con;
+		}catch(ClassNotFoundException e) {
+			throw new SQLException(e);
+		}
+	}
+
+
+
+	public static int simpleUpdate(String sql) throws SQLException{
+
+		Connection con =null;
+		Statement smt=null;
+		try {
+			con=getConnection();
+			smt=con.createStatement();
+			return smt.executeUpdate(sql);
+		}finally {
+			if(smt != null) {
+				try {
+				smt.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
+	public static <T> List<T> findAll(String sql, ResultSetBeanMapping<T> mapping)
+		throws SQLException{
+
+		Connection con = null;
+		Statement smt = null;
+
+		try {
+			con=getConnection();
+			smt=con.createStatement();
+			ResultSet rs=smt.executeQuery(sql);
+
+			List<T> list=new ArrayList<T>();
+			while (rs.next()) {
+				T bean=mapping.createFromResultSet(rs);
+				list.add(bean);
+			}
+			return list;
+		}finally {
+			if(smt != null) {
+				try {
+					con.close();
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 
 }
--- newproject/WEB-INF/src/resultmapping/PriceMapping.java
+++ newproject/WEB-INF/src/resultmapping/PriceMapping.java
@@ -1,5 +1,20 @@
 package resultmapping;
 
-public class PriceMapping {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import bean.Price;
+
+public class PriceMapping implements ResultSetBeanMapping<Price>{
+
+	@Override
+	public Price createFromResultSet(ResultSet rs) throws SQLException {
+		// TODO 自動生成されたメソッド・スタブ
+		Price price = new Price();
+		price.setStockCode(rs.getInt("stock_code"));
+		
+		price.setVolume(rs.getInt("volume"));
+		return price;
+	}
+	
 }
