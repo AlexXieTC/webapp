c2b471fe67edd6c1aa8c80c5e9a478075f960f96
髙田 稜太 <qcq1725@quick.local>
2020-06-30 14:39:09 +0900

    bean(Asset, Ranking)の追加：高田

---
+++ newproject/WEB-INF/src/bean/Asset.java
@@ -0,0 +1,46 @@
+package bean;
+
+public class Asset {
+
+		private String userID;
+		private String stockCode;
+		private int number;
+		
+
+		public Asset() {
+	
+		}
+
+
+		public String getUserID() {
+			return userID;
+		}
+
+
+		public void setUserID(String userID) {
+			this.userID = userID;
+		}
+
+
+		public String getStockCode() {
+			return stockCode;
+		}
+
+
+		public void setStockCode(String stockCode) {
+			this.stockCode = stockCode;
+		}
+
+
+		public int getNumber() {
+			return number;
+		}
+
+
+		public void setNumber(int number) {
+			this.number = number;
+		}
+		
+		
+
+}
---
+++ newproject/WEB-INF/src/bean/Ranking.java
@@ -0,0 +1,40 @@
+package bean;
+
+import java.sql.Date;
+
+public class Ranking {
+
+	private String userID;
+	private int money;
+	private Date date;
+
+	public Ranking() {
+
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+}
