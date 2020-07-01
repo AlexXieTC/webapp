67597772fc9dc4e65264272805a659afaf584b4e
y-hikino <y-hikino@vcl.jp>
2020-07-01 12:17:58 +0900

    髙田君のファイルのマージ

---
+++ newproject/WEB-INF/src/dao/registrationDAO.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.SQLException;
+
+import bean.User;
+import database.DBManager;
+
+public class registrationDAO {
+	public static void main(String[] args) throws SQLException {
+		User user = new User();
+		user.setId("admin");
+		user.setPassword("adm1");
+		insert(user);
+	}
+	public static int insert(User user) throws SQLException{
+
+//		String sql= "INSERT INTO userinformation"+"(user_id,password,money,simulation_date)
+//						VALUES("+"'"+user.getId() +"',"+"'"+ user.getPassword()+"',"+ 1000000,"'"+2020/06/01"'"+)";
+
+		String sql="INSERT INTO userinformation" +"(user_id,password,money,simulation_date) VALUES(" +
+			"'"+user.getId()+"',"+"'"+user.getPassword()+"',1000000,'2020/06/01')";
+
+		System.out.println(sql);
+		return DBManager.simpleUpdate(sql);
+	}
+}
