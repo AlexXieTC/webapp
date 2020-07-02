759aa6e545a8a8b3f20703bef0935a04cd596d8c
y-hikino <y-hikino@vcl.jp>
2020-07-02 11:47:42 +0900

    売却処理用サーブレットの更新

--- newproject/WEB-INF/src/Servlet/SellServlet.java
+++ newproject/WEB-INF/src/Servlet/SellServlet.java
@@ -1,7 +1,52 @@
 package Servlet;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Asset;
+import bean.User;
+
+@WebServlet("/sell")
 public class SellServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO 自動生成されたメソッド・スタブ
+
+		String forwardURL ="/stock/error.jsp";
+
+
+		//RequestScopeから更新後の情報を持つbeanを取得
+		User uBean = (User)req.getAttribute("user?");
+		Asset aBean = (Asset)req.getAttribute("asset");
+
+		boolean isSuccess =false;
+		try {
+
+			isSuccess = dao.SellDAO.update(uBean, aBean);
+
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		if(isSuccess) {
+			forwardURL ="stock/success.jsp";
+
+			HttpSession session = req.getSession();
+			session.setAttribute("user?", uBean);
+
+			//sell処理と使いまわすのならメッセージを追加するべき？
+
+		}
+
+		req.getRequestDispatcher(forwardURL).forward(req, resp);
+	}
 
 }
