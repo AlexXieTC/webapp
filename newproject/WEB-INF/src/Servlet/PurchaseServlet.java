9cae6cd61a05a553f330773e5ce3cc0435391812
y-hikino <y-hikino@vcl.jp>
2020-07-02 11:32:57 +0900

    purchaseServletの名前修正

--- newproject/WEB-INF/src/Servlet/PurchaseServlet.java
+++ newproject/WEB-INF/src/Servlet/PurchaseServlet.java
@@ -3,23 +3,30 @@ package Servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import bean.Asset;
 import bean.User;
 
-
-public class ParchaseServlet extends HttpServlet {
+@WebServlet("/purchase")
+public class PurchaseServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO 自動生成されたメソッド・スタブ
-		//AssetをrequestScope
-		HttpSession session = req.getSession();
 
-		User Bean = (User)req.getAttribute("user?");
+		//RequestScopeから更新後の情報を持つbeanを取得
+		User uBean = (User)req.getAttribute("user?");
 		Asset aBean = (Asset)req.getAttribute("asset");
+
+		boolean isInsert = (Boolean)req.getAttribute("isInsert");
+
+		boolean isSuccess =
+
+
+
+
 	}
 }
