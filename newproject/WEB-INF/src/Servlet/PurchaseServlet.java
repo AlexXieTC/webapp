0fb4e8f15e8101bf5b77ddffe769ecc7ac1742aa
y-hikino <y-hikino@vcl.jp>
2020-07-02 11:40:57 +0900

    doPost処理の更新

--- newproject/WEB-INF/src/Servlet/PurchaseServlet.java
+++ newproject/WEB-INF/src/Servlet/PurchaseServlet.java
@@ -1,12 +1,14 @@
 package Servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bean.Asset;
 import bean.User;
@@ -17,14 +19,37 @@ public class PurchaseServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO 自動生成されたメソッド・スタブ
 
+		String forwardURL ="/stock/error.jsp";
+
+
 		//RequestScopeから更新後の情報を持つbeanを取得
 		User uBean = (User)req.getAttribute("user?");
 		Asset aBean = (Asset)req.getAttribute("asset");
 
 		boolean isInsert = (Boolean)req.getAttribute("isInsert");
 
-		boolean isSuccess =
+		boolean isSuccess =false;
+		try {
 
+			if(isInsert)isSuccess = dao.PurchaseDAO.insert(uBean, aBean);
+			else isSuccess = dao.PurchaseDAO.update(uBean, aBean);
+
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		if(isSuccess) {
+			forwardURL ="stock/success.jsp";
+
+			HttpSession session = req.getSession();
+			session.setAttribute("user?", uBean);
+
+			//sell処理と使いまわすのならメッセージを追加するべき？
+
+		}
+
+		req.getRequestDispatcher(forwardURL).forward(req, resp);
 
 
 
